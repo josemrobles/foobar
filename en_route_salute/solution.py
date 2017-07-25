c7357b35721e6611f29d04e160668341bbfc9fb6
josemrobles <das.haas@gmail.com>
2017-07-25 09:37:28 -0400

    input validation

--- en_route_salute/solution.py
+++ en_route_salute/solution.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python
 
+def validateInput(s):
+	if ( 1 <= len(s) <= 100):
+		return True
+	else:
+		print len(s)
+		return False
+
 def answer (s):
-		
-	l = list(s)
-	#a = [i for i in l if i is not "-"]
-	a_ = [i for i in l if i in ["<",">"]]
+	
+	if validateInput(s) is True:		
+		l = list(s)
+		a = [i for i in l if i in ["<",">"]]
 
-	print l
+		#for x in a:
+		#	print x
 
-	for x in l:
-		print x
+		return a
 
-	return a_
-
-print answer("<<->><000")
+print answer("<<>><")
