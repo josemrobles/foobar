f6e799d00e8cde5dac2ff7f585ef669a3b1d4433
josemrobles <das.haas@gmail.com>
2017-07-25 13:48:48 -0400

    getting there

--- en_route_salute/solution.py
+++ en_route_salute/solution.py
@@ -7,20 +7,22 @@ def validateInput(s):
 		print len(s)
 		return False
 
-def calcLeft(d):
+def calcLeft(a):
 	count = 0
-	for r1 in d['right']:
-		for l1 in d['left']:
-			if r1 > l1:
-				count += 1
+	for k,v in enumerate(a):
+		if v == ">":
+			for k_,v_ in enumerate(a):
+				if (k_ > k and v_ == "<"):
+					count += 1
 	return count
 
-def calcRight(d):
+def calcRight(a):
 	count = 0
-	for r1 in d['right']:
-		for l1 in d['left']:
-			if r1 > l1:
-				count += 1
+	for k,v in enumerate(a):
+		if v == "<":
+			for k_,v_ in enumerate(a):
+				if (k_ < k and v_ == ">"):
+					count += 1
 	return count
 
 def answer (s):
@@ -28,16 +30,5 @@ def answer (s):
 	if validateInput(s) is True:		
 		l = list(s)
 		a = [i for i in l if i in ["<",">"]]
-		d = {'right': [], 'left': []}
 
-		for x in a:
-			if x == ">":
-				d['right'].append(x)
-
-			if x == "<":
-				d['left'].append(x)			
-
-		return calcLeft(d) + calcRight(d)
-
-print answer("<<>><")
-#print answer("--->-><-><-->-")
+		return calcLeft(a) + calcRight(a)
