93e2b24a0a1d314f8eb633a7ae02f61cdcafa64f
josemrobles <das.haas@gmail.com>
2017-07-26 12:44:10 -0400

    meh

--- en_route_salute/solution.py
+++ en_route_salute/solution.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+import operator
 
 def validateInput(s):
 	if ( 1 <= len(s) <= 100):
@@ -7,28 +7,26 @@ def validateInput(s):
 		print len(s)
 		return False
 
-def calcLeft(a):
+def calcSalutes(a,b,r):
 	count = 0
 	for k,v in enumerate(a):
-		if v == ">":
+		if v == b:
 			for k_,v_ in enumerate(a):
-				if (k_ > k and v_ == "<"):
+				if (r(k_, k) and v_ == flip(b)):
 					count += 1
 	return count
 
-def calcRight(a):
-	count = 0
-	for k,v in enumerate(a):
-		if v == "<":
-			for k_,v_ in enumerate(a):
-				if (k_ < k and v_ == ">"):
-					count += 1
-	return count
+def flip(s):
+	if s == ">":
+		return "<"
+	else:
+		return ">"
 
 def answer (s):
-	
 	if validateInput(s) is True:		
 		l = list(s)
 		a = [i for i in l if i in ["<",">"]]
 
-		return calcLeft(a) + calcRight(a)
+		return calcSalutes(a,"<",operator.lt) + calcSalutes(a,">",operator.gt)
+
+print answer("--->-><-><-->-")
